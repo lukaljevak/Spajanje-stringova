f8a2a7b64018245bdd9e90bc0880e2939558e82b
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-01-16 12:47:12 +0100

    Add files via upload

---
+++ Program4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rečenicu:");
+            string recenica = Console.ReadLine();
+
+            string[] rijeci = recenica.Split(' ');
+
+            string prvaRijec = rijeci[0];
+            string zadnjaRijec = rijeci[rijeci.Length - 1];
+
+            Console.WriteLine("Prva riječ: " + prvaRijec);
+            Console.WriteLine("Zadnja riječ: " + zadnjaRijec);
+
+            Console.ReadLine();
+        }
+    }
+}
